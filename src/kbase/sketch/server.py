cdbbc769e37d56157cd279fee557e41c8e4af1f9
Jay R Bolton <jayrbolton@gmail.com>
2018-08-07 15:51:31 -0700

    Update the API to use JSON RPC 1.1

--- src/kbase/sketch/server.py
+++ src/kbase/sketch/server.py
@@ -1,3 +1,4 @@
+import json
 import tempfile
 import os
 import shutil
@@ -17,23 +18,26 @@ app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', True)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def root():
-    """Root path for the entire service"""
-    return "Usage: GET /search/workspace_id. Example: GET /search/1/2/3"
-
-
-@app.route('/search/<ws_id>/<obj_id>/<obj_ver>', methods=['GET'])
-def get_sketch(ws_id, obj_id, obj_ver):
-    """Generate a sketch from a given genome."""
-    ws_ref = '/'.join([ws_id, obj_id, obj_ver])
+    """
+    JSON RPC v1.1 method call.
+    Reference: https://jsonrpc.org/historical/json-rpc-1-1-wd.html
+    We ignore the "method" parameter here as this service has only one function.
+    """
+    json_data = flask.request.get_json()
+    ws_ref = json_data['params'][0]
     # For each sketch, we make and remove a temporary directory
     tmp_dir = tempfile.mkdtemp()
     (path, paired_end) = autodownload(ref=ws_ref, save_dir=tmp_dir)
     sketch_result = generate_sketch(path, paired_end)
     search_result = perform_search(sketch_result)
     shutil.rmtree(tmp_dir)
-    return search_result
+    return flask.jsonify({
+        'version': '1.1',
+        'id': json_data.get('id'),
+        'result': json.loads(search_result)
+    })
 
 
 @app.errorhandler(InvalidUser)
@@ -42,17 +46,17 @@ def get_sketch(ws_id, obj_id, obj_ver):
 @app.errorhandler(InaccessibleWSObject)
 def invalid_user(err):
     """Generic exception catcher, returning 400."""
-    return (flask.jsonify({'status': 'error', 'message': str(err)}), 400)
+    return (flask.jsonify({'version': '1.1', 'error': str(err)}), 400)
 
 
 @app.errorhandler(404)
 def page_not_found(err):
-    return (flask.jsonify({'status': 'error', 'message': 'Page not found'}), 404)
+    """Return a JSON data for 404."""
+    return (flask.jsonify({'version': '1.1', 'error': 'Page not found'}), 404)
 
 
 @app.errorhandler(Exception)
 def any_exception(err):
     """A catch-all for any exceptions we didn't handle above."""
-    print(str(Exception))
     traceback.print_exc()
-    return (flask.jsonify({'status': 'error', 'message': 'An unknown error occurred'}), 500)
+    return (flask.jsonify({'version': '1.1', 'error': str(err)}), 500)
--- src/kbase/sketch/test/test_api.py
+++ src/kbase/sketch/test/test_api.py
@@ -3,83 +3,73 @@ Simple integration tests on the API itself.
 
 We make actual ajax requests to the running docker container.
 """
-
+import json
 import unittest
 import requests
 
-# This must be the URL of the running server from within the docker container
+# The URL of the running server from within the docker container
 url = 'http://0.0.0.0:5000'
+
+
+def make_request(ws_ref):
+    """Helper to make a JSON RPC request with the given workspace ref."""
+    post_data = {'params': [ws_ref], 'method': 'get_homologs', 'id': 0}
+    resp = requests.post(url, data=json.dumps(post_data),
+                         headers={'Content-Type': 'application/json'})
+    return resp.json()
 
 
 class TestApi(unittest.TestCase):
 
-    def test_root(self):
-        """Test the root endpoint."""
-        resp = requests.get(url)
-        # Don't particularly feel the need to test the content of this
-        self.assertTrue(len(resp.text))
-
     def test_search_reads_paired(self):
         """Test a search on genome read data with paired-ends."""
         reads_ref = '15/45/1'
-        full_url = url + '/search/' + reads_ref
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertTrue(len(json['distances']))
+        json_resp = make_request(reads_ref)
+        result = json_resp['result']
+        self.assertTrue(len(result['distances']))
 
     def test_search_reads_single(self):
         """Test a search on single-ended genome read data."""
         reads_ref = '15/43/1'
-        full_url = url + '/search/' + reads_ref
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertTrue(len(json['distances']))
+        json_resp = make_request(reads_ref)
+        result = json_resp['result']
+        self.assertTrue(len(result['distances']))
 
     def test_search_genome(self):
         """Test a search on a Genome type."""
         genome_ref = '34819/14/1'
-        full_url = url + '/search/' + genome_ref
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertTrue(len(json['distances']))
+        json_resp = make_request(genome_ref)
+        result = json_resp['result']
+        self.assertTrue(len(result['distances']))
 
     def test_search_assembly(self):
         """Test a search on an Assembly type."""
         assembly_ref = '34819/10/1'
-        full_url = url + '/search/' + assembly_ref
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertTrue(len(json['distances']))
+        json_resp = make_request(assembly_ref)
+        result = json_resp['result']
+        self.assertTrue(len(result['distances']))
 
     def test_search_assembly_contigset(self):
         """Test a search on an Assembly with the legacy ContigSet."""
         assembly_ref = '34819/8/1'
-        full_url = url + '/search/' + assembly_ref
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertTrue(len(json['distances']))
+        json_resp = make_request(assembly_ref)
+        result = json_resp['result']
+        self.assertTrue(len(result['distances']))
 
     def test_search_genome_no_assembly_ref(self):
         """Test a failed search against a Genome that has no assembly_ref or contigset_ref."""
         genome_ref = '34819/5/9'
-        full_url = url + '/search/' + genome_ref
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertEqual(json['status'], 'error')
-        self.assertTrue('no assembly or contigset references' in json['message'])
+        json_resp = make_request(genome_ref)
+        self.assertTrue('no assembly or contigset references' in json_resp['error'])
 
     def test_search_invalid_ws_id(self):
         """Test a search with an invalid workspace reference ID."""
-        full_url = url + '/search/x/y/z'
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertEqual(json['status'], 'error')
-        self.assertTrue(len(json['message']))
+        ref = 'x/y/z'
+        json_resp = make_request(ref)
+        self.assertTrue(len(json_resp['error']))
 
     def test_search_unauthorized_ws_id(self):
         """Test a search with an unauthorized workspace ref."""
-        full_url = url + '/search/1/2/3'
-        resp = requests.get(full_url)
-        json = resp.json()
-        self.assertEqual(json['status'], 'error')
-        self.assertTrue(len(json['message']))
+        ref = '/search/1/2/3'
+        json_resp = make_request(ref)
+        self.assertTrue(len(json_resp['error']))
