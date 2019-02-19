5499a4cdb4873f3c5ce75fa7b014f11235e24dd0
slebras <slebras@Sebastians-MBP.dhcp.lbnl.us>
2019-02-19 09:58:02 -0800

    Adding refseq strain name to sketch_service for now, work on moving to id mapping service soonish

--- src/utils/map_strains.py
+++ src/utils/map_strains.py
@@ -1,7 +1,16 @@
+import pandas as pd
+
 
 def map_strains(distances):
     # TODO insert strain names here
     print('distances', distances)
+    df = pd.read_csv('data/gcf_strain_map.csv')
+    refseq_ids = [d['sourceid'] for d in distances]
+    df = df[df['refseq_id'].isin(reseq_ids)]
+    d = dict(zip(df['refseq_id'], df['strain']))
     for dist in distances:
-        dist['strain'] = 'TODO'
+        if dist['sourceid'] not in d:
+            dist['strain'] = None
+        else:
+            dist['strain'] = d[dist['sourceid']]
     return distances
