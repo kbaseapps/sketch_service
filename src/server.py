5b4995e670060d05fd40c7a03701194b3149972c
slebras <slebras@lbl.gov>
2019-11-19 10:13:31 -0800

    adding bypass to caching

--- src/server.py
+++ src/server.py
@@ -51,6 +51,7 @@ def get_homologs(json_data, auth_token):
     if not params.get('ws_ref'):
         raise RuntimeError("params must contain 'ws_ref' (a workspace reference to an object)")
     n_max_results = params.get('n_max_results', 10)
+    bypass_caching = params.get('bypass_caching', False)
     # n_max_results argument must be an integer
     if not isinstance(n_max_results, int):
         raise RuntimeError("n_max_results must be an integer")
@@ -61,16 +62,24 @@ def get_homologs(json_data, auth_token):
     # Create unique identifying data for the cache
     cache_data = {'ws_ref': ws_ref, 'db_name': search_db, 'fn': 'get_homologs', 'n_max_results': n_max_results}
     cache_id = get_cache_id(cache_data)
-    try:
-        cached = download_cache_string(cache_id)
-        search_result = json.loads(cached)
-    except Exception:
+
+    def sketch_search_cache(cache_):
         # If it is not cached, then we generate the sketch, perform the search, and cache it
         (data_path, paired_end) = autodownload(ws_ref, tmp_dir, auth_token)
         sketch_path = generate_sketch(data_path, search_db, paired_end)
         search_result = perform_search(sketch_path, search_db, n_max_results)
-        if search_result:
+        if search_result and cache_:
             upload_to_cache(cache_id, json.dumps(search_result))
+        return search_result
+
+    if bypass_caching:
+        search_result = sketch_search_cache(False)
+    else:
+        try:
+            cached = download_cache_string(cache_id)
+            search_result = json.loads(cached)
+        except Exception:
+            search_result = sketch_search_cache(True)
     shutil.rmtree(tmp_dir)  # Clean up all temp files
     return flask.jsonify({
         'version': '1.1',
